eeeea35e38e90ce2924eb1a8dc15bad0b2410e25
Lalit Gupta <lg989212@gmail.com>
2025-06-03 12:04:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+  import React,{useEffect, useState} from 'react';
+  import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
 
-export default App;
+    const[fact,setFact]=useState('');
+    const[loading,setLoading]=useState(false);
+    const[img,setImg]=useState('');
+    const fetchFact =async()=>{
+      setLoading(true);
+      try{
+        const response=await fetch('https://catfact.ninja/fact');
+        const image1 =await fetch('https://api.thecatapi.com/v1/images/search');
+        const imagedata = await image1.json();
+        const data =await response.json();
+        setFact(data.fact);
+        
+        setImg(imagedata[0].url);
+      }
+      catch(error){
+        setFact("Failed to fetch a fact. Please try again later....");
+      }
+      setLoading(false);
+    };
+
+    useEffect(()=>{
+      console.log(fact);
+    },[fact])
+
+    return (
+      <div className='body'>
+        <button onClick={fetchFact} >
+          Get a Random Fact.
+        </button>
+        <p>{loading? 'Loading...' : fact} </p>
+        {img && <img src={img} alt="Cat" width="300" style={{ marginTop: '20px' }} />}
+      </div>
+    );
+  }
+
+  export default App;
